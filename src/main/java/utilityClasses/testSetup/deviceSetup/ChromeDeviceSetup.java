2cd24eca7198af850d5ed0567f39aa390417dc52
norbert-s <susztek.norbert@gmail.com>
2022-08-07 21:01:48 +0200

    settings

--- src/main/java/utilityClasses/testSetup/deviceSetup/ChromeDeviceSetup.java
+++ src/main/java/utilityClasses/testSetup/deviceSetup/ChromeDeviceSetup.java
@@ -6,11 +6,8 @@ import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
-
 import utilityClasses.testSetup.GlobalSettingsGetterMethods;
-
 import java.io.File;
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
@@ -22,24 +19,16 @@ public class ChromeDeviceSetup implements DeviceSetupBase {
     @Override
     public  ChromeOptions settipUpDevices(String browser) throws Exception {
         chromeOptions =new ChromeOptions();
-
-
             if (browser.toLowerCase().contains("chrome".toLowerCase(Locale.ROOT))) {
-
-
-                //chromeOptions.addArguments("--window-size=1920,1080");
-//                    if(GlobalSettingsGetterMethods.isXNoCacheNeeded()){
-//                        chromeOptions.addArguments("--disable-web-security");
-//                    }
-                //chromeOptions.addArguments("--incognito");
-                chromeOptions.addArguments("--disable-notifications");
+                if(GlobalSettingsGetterMethods.isIncognitoNeeded()){
+                    chromeOptions.addArguments("--incognito");
+                }
                 chromeOptions.addArguments("start-maximized");
                 chromeOptions.setHeadless(GlobalSettingsGetterMethods.getHeadless());
             }
             if (browser.contains("ipad")) {
                 Map<String, String> mobileEmulation = new HashMap<>();
                 mobileEmulation.put("deviceName", "iPad Pro");
-
                 HashMap<String, Object> prefs = new HashMap<>();
                 chromeOptions.addArguments("--incognito");
                 if (GlobalSettingsGetterMethods.isXNoCacheNeeded()) {
@@ -48,7 +37,6 @@ public class ChromeDeviceSetup implements DeviceSetupBase {
                 chromeOptions.setExperimentalOption("prefs", prefs);
                 chromeOptions.setExperimentalOption("mobileEmulation", mobileEmulation);
                 chromeOptions.setHeadless(GlobalSettingsGetterMethods.getHeadless());
-
             }
 
         return chromeOptions;
